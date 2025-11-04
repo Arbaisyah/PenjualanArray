e7d97cb0119b4a3609c6a0f195de5707ef76f603
Siti Arbaisyah <sitiarbaisyah0106@gmail.com>
2025-11-04 15:37:06 +0700

    Commit 1-setup awal

---
+++ array1.php
@@ -0,0 +1,10 @@
+<?php
+
+  echo "<h2>-- POLGAN MART --</h2>";
+
+     $kode_barang  = ["B001", "B002", "B003", "B004", "B005"];
+     $nama_barang  = ["Pensil", "Buku Tulis", "Penghapus", "Pulpen", "Spidol"];
+     $harga_barang = [2000, 5000, 1500, 3000, 4500];
+
+
+?>
