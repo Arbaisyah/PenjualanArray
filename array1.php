219d53c3df6b0280511a24e22874e86be5e37202
Siti Arbaisyah <sitiarbaisyah0106@gmail.com>
2025-11-04 16:12:09 +0700

    commit 1 - logika pembelian

--- array1.php
+++ array1.php
@@ -1,12 +1,32 @@
 <?php
+echo "<h2>-- POLGAN MART --</h2>";
 
+// Data barang (array multidimensi biar rapi)
+$barang = [
+    ["B001", "Pensil", 2000],
+    ["B002", "Buku Tulis", 5000],
+    ["B003", "Penghapus", 1500],
+    ["B004", "Pulpen", 3000],
+    ["B005", "Spidol", 4500],
+];
 
-  //tampilan header
-  echo "<h2>-- POLGAN MART --</h2>";
+// Acak urutan daftar barang
+shuffle($barang);
 
-     $kode_barang  = ["B001", "B002", "B003", "B004", "B005"];
-     $nama_barang  = ["Pensil", "Buku Tulis", "Penghapus", "Pulpen", "Spidol"];
-     $harga_barang = [2000, 5000, 1500, 3000, 4500];
+// Tampilkan daftar barang yang sudah diacak
+echo "<h3>Daftar Barang :</h3>";
+echo "<table border='1' cellpadding='5' cellspacing='0'>";
+echo "<tr><th>Kode</th><th>Nama Barang</th><th>Harga (Rp)</th></tr>";
 
+foreach ($barang as $b) {
+    echo "<tr>
+            <td>{$b[0]}</td>
+            <td>{$b[1]}</td>
+            <td>{$b[2]}</td>
+          </tr>";
+}
 
+echo "</table>";
 ?>
+
+
