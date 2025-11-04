275a263cf13e9da13a14a8a2072817fa6d847cca
Siti Arbaisyah <sitiarbaisyah0106@gmail.com>
2025-11-04 16:26:12 +0700

    commit 3 - perhitungan total

--- array1.php
+++ array1.php
@@ -1,7 +1,7 @@
 <?php
 echo "<h2>-- POLGAN MART --</h2>";
 
-// Data barang (array multidimensi biar rapi)
+// Data barang
 $barang = [
     ["B001", "Pensil", 2000],
     ["B002", "Buku Tulis", 5000],
@@ -10,23 +10,50 @@ $barang = [
     ["B005", "Spidol", 4500],
 ];
 
-// Acak urutan daftar barang
+// Acak urutan barang
 shuffle($barang);
 
-// Tampilkan daftar barang yang sudah diacak
-echo "<h3>Daftar Barang :</h3>";
-echo "<table border='1' cellpadding='5' cellspacing='0'>";
-echo "<tr><th>Kode</th><th>Nama Barang</th><th>Harga (Rp)</th></tr>";
+// Variabel untuk menyimpan total-total
+$beli = [];
+$total = [];
+$grand_total = 0;
 
+// Tampilkan tabel
+echo "<h3>Daftar Barang Yg dibeli:</h3>";
+echo "<table border='1' cellpadding='5' cellspacing='0'>";
+echo "<tr>
+        <th>Kode</th>
+        <th>Nama Barang</th>
+        <th>Harga (Rp)</th>
+        <th>Jumlah Beli</th>
+        <th>Total (Rp)</th>
+      </tr>";
+
+// Loop tampilkan data barang + beli acak
 foreach ($barang as $b) {
+    $jumlah_beli = rand(1, 10); // jumlah acak
+    $subtotal = $b[2] * $jumlah_beli;
+
+    // Simpan data
+    $beli[] = $jumlah_beli;
+    $total[] = $subtotal;
+    $grand_total += $subtotal;
+
+    // Tampilkan baris barang
     echo "<tr>
             <td>{$b[0]}</td>
             <td>{$b[1]}</td>
-            <td>{$b[2]}</td>
+            <td align='right'>{$b[2]}</td>
+            <td align='center'>$jumlah_beli</td>
+            <td align='right'>$subtotal</td>
           </tr>";
 }
 
+// Tambahkan baris Grand Total di dalam tabel
+echo "<tr style='background-color: #f2f2f2; font-weight: bold;'>
+        <td colspan='4' align='center'>Grand Total</td>
+        <td align='right'>Rp $grand_total</td>
+      </tr>";
+
 echo "</table>";
 ?>
-
-
